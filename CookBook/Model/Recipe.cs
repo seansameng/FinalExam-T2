28c82a208f976cb26f1fcc2325bd20b8b131c0ad
accer lmc <accer lmc@DESKTOP-SO0SK4C>
2024-02-14 13:21:55 +0700

    Add project files.

---
+++ CookBook/Model/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Model
+{
+    public class Recipe
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<string> Images { get; set; } = [];
+        public List<string> Ingredients { get; set; } = [];
+        public string Instructions { get; set; } = "";
+
+    }
+}
---
+++ CookBook/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CookBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = "";
+
+        private CuisineList cuisineList = new CuisineList();
+
+        [ObservableProperty]
+        private ObservableCollection<Cuisine> cuisines;
+
+        [ObservableProperty]
+        private Cuisine selectedCuisine;
+        public MainViewModel()
+        {
+            cuisines = new ObservableCollection<Cuisine>(cuisineList.Cuisines);
+            SelectedCuisine = cuisines[0];
+        }
+    }
+
+
+}
